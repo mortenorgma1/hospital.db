4174e66e50b80ae5860bcc3464a6792ea3e0eee4
Morten Orgma <mortenorgma1@gmail.com>
2026-01-07 14:21:36 +0100

    Schema.sql

--- Schema.sql
+++ Schema.sql
@@ -1,3 +1,31 @@
-SELECT table_name
-FROM information_schema.tables
-WHERE table_schema = 'public';
+CREATE TABLE provinces (
+    province_id SERIAL PRIMARY KEY,
+    province_name TEXT NOT NULL
+);
+
+CREATE TABLE patients (
+    patient_id SERIAL PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    gender CHAR(1),
+    birth_date DATE,
+    height INTEGER,
+    weight INTEGER,
+    province_id INTEGER REFERENCES provinces(province_id)
+);
+
+CREATE TABLE doctors (
+    doctor_id SERIAL PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    specialty TEXT NOT NULL
+);
+
+CREATE TABLE admissions (
+    admission_id SERIAL PRIMARY KEY,
+    patient_id INTEGER REFERENCES patients(patient_id),
+    attending_doctor_id INTEGER REFERENCES doctors(doctor_id),
+    admission_date DATE NOT NULL,
+    discharge_date DATE,
+    diagnosis TEXT
+);
