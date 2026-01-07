ea6ed436eb84a794812d2acf07b5f4859efc091a
Morten Orgma <mortenorgma1@gmail.com>
2026-01-07 14:28:30 +0100

    Create 02_daily_admission_change.sql

---
+++ sql/02_daily_admission_change.sql
@@ -0,0 +1,15 @@
+WITH daily_admissions AS (
+    SELECT
+        admission_date,
+        COUNT(*) AS total_admissions
+    FROM admissions
+    GROUP BY admission_date
+)
+SELECT
+    admission_date,
+    total_admissions,
+    total_admissions
+        - LAG(total_admissions) OVER (ORDER BY admission_date)
+        AS change_from_previous_day
+FROM daily_admissions
+ORDER BY admission_date;
