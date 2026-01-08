e997f36e25bee7bdd1c76c3deedcf7d05e291b38
Morten Orgma <mortenorgma1@gmail.com>
2026-01-08 14:55:34 +0100

    Update 01_daily_admissions.sql

--- sql/01_daily_admissions.sql
+++ sql/01_daily_admissions.sql
@@ -1,3 +1,12 @@
+-- Calculates total hospital admissions per day
+SELECT
+    admission_date,
+    COUNT(*) AS total_admissions
+FROM admissions
+GROUP BY admission_date
+ORDER BY admission_date;
+
+
 SELECT
     admission_date,
     COUNT(*) AS total_admissions
