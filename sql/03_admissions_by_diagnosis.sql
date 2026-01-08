f00f332ca99d7ee31eb7b02dbedccae23836d85b
Morten Orgma <mortenorgma1@gmail.com>
2026-01-08 14:58:06 +0100

    Update 03_admissions_by_diagnosis.sql

--- sql/03_admissions_by_diagnosis.sql
+++ sql/03_admissions_by_diagnosis.sql
@@ -1,10 +1,9 @@
-/*
-Shows total number of admissions per diagnosis
-Used to identify the most common medical conditions
-*/
+-- Shows total number of admissions per diagnosis Used to identify the most common medical conditions
+
 SELECT
     diagnosis,
     COUNT(*) AS total_admissions
 FROM admissions
 GROUP BY diagnosis
 ORDER BY total_admissions DESC;
+
